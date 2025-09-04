645fafc0167d8469be24ec973c17cf96ef69ec1f
FunEnn <3095852337@qq.com>
2025-09-04 23:33:57 +0800

    builud: github action

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 export const fetchAPKData = async () => {
-  const response = await fetch('https://funenn.github.io/apk-collection/apkData.json');
+  const response = await fetch('https://funenn.github.io/StashAPK/apkData.json');
   if (!response.ok) throw new Error('Failed to fetch APK data');
   return response.json();
 };
