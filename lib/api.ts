a8e6d3570529d3d1cff63b8201b9b749e86969fc
FunEnn <3095852337@qq.com>
2025-09-15 20:06:14 +0800

    ci: 工作流修改

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,7 @@
+const API_URL = process.env.EXPO_PUBLIC_API_URL || 'https://funenn.github.io/StashAPK/apkData.json';
+
 export const fetchAPKData = async () => {
-  const response = await fetch('https://funenn.github.io/StashAPK/apkData.json');
+  const response = await fetch(`${API_URL}`);
   if (!response.ok) throw new Error('Failed to fetch APK data');
   return response.json();
 };
